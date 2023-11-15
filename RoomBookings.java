348a823e73fa9e4a26b6291232c50b6b17544658
191ahsan <116483532+191ahsan@users.noreply.github.com>
2023-11-15 14:58:19 -0400

    Update RoomBookings.java

---
+++ RoomBookings.java
@@ -0,0 +1,55 @@
+public class RoomBookings{
+    private int bookingID;
+    private int memberID;
+    private boolean available;
+    private boolean hasPC;
+    private int rmNum;
+    private int duration;
+    public RoomBookings(int bookingID, int memberID, boolean available, boolean hasPC,int rmNum, int duration){
+        bookingID=this.bookingID;
+        memberID=this.memberID;
+        available=this.available;
+        hasPC=this.hasPC;
+        rmNum=this.rmNum;
+        duration=this.duration;
+    }
+    public int getBookingID() {
+        return bookingID;
+    }
+    public void setBookingID(int bookingID) {
+        this.bookingID = bookingID;
+    }
+    public int getMemberID() {
+        return memberID;
+    }
+    public void setMemberID(int memberID) {
+        this.memberID = memberID;
+    }
+    public boolean isAvailable() {
+        return available;
+    }
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+    public boolean isHasPC() {
+        return hasPC;
+    }
+    public void setHasPC(boolean hasPC) {
+        this.hasPC = hasPC;
+    }
+    public int getRmNum() {
+        return rmNum;
+    }
+    public void setRmNum(int rmNum) {
+        this.rmNum = rmNum;
+    }
+    public int getDuration() {
+        return duration;
+    }
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+    
+}
+
+

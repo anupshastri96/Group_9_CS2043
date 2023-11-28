1c405273a8a2414f76cd6db06848d74cf1270568
AdamC-4 <145706026+AdamC-4@users.noreply.github.com>
2023-11-28 13:12:10 -0400

    added sorting methods to Book.java

--- Book.java
+++ Book.java
@@ -1,6 +1,9 @@
 /**	This class represents a Book
 	@author Ben Mozgovoi 3742139
 */
+import java.util.HashMap;
+//import com.mysql.jdbc.Statement;
+import java.sql.*;
 public class Book{
 
     private static int ID = 100;
@@ -42,7 +45,161 @@ public class Book{
         title = titleIn;
 
     }
-
+	public static void getAll()
+	{
+	try
+		{
+		String url = "jdbc:mysql://54.84.79.252:3306/sql5664279";
+		Connection connector = DriverManager.getConnection(url,"sql5664279","BD4wVguFkr");
+		Statement stmt = connector.createStatement();
+         	ResultSet rs = stmt.executeQuery("SELECT * FROM books");	
+         	System.out.printf("%-4s | %-35s | %-15s | %-15s | %-25s | %-8s | %-11s | %-6s | %-12s | %-26s | %-25s\n","ID","Title","Last Name","First Name","Genre","Quantity","Quality","Rating","ReleaseYear","Publisher/Studio","Illustrator");
+         
+         while (rs.next()) {
+            int id = rs.getInt("bookID");
+            String title = rs.getString("title");
+            String lName = rs.getString("authorLastName");
+            String fName = rs.getString("authorFirstName");
+            String gen = rs.getString("genre");
+            int quan = rs.getInt("quantity");
+            String qual = rs.getString("quality");
+            float rat = rs.getFloat("rating");
+            int relY = rs.getInt("releaseYear");
+            String pubS = rs.getString("publisherStudio");
+            String ill = rs.getString("illustrator");
+            System.out.printf("%-4s | %-35s | %-15s | %-15s | %-25s | %-8s | %-11s | %-6s | %-12s | %-26s | %-25s\n",id, title, lName, fName, gen, quan, qual, rat, relY, pubS, ill);
+            	}
+	        connector.close();
+      		}
+      catch(SQLException e)
+		{
+			System.out.println("Database Error: "+e.getMessage());
+		}
+	}
+	public static void genreSort()
+	{
+	try
+		{
+		String url = "jdbc:mysql://54.84.79.252:3306/sql5664279";
+		Connection connector = DriverManager.getConnection(url,"sql5664279","BD4wVguFkr");
+		Statement stmt = connector.createStatement();
+         	ResultSet rs = stmt.executeQuery("SELECT * FROM books order by genre");	
+         	System.out.printf("%-4s | %-35s | %-15s | %-15s | %-25s | %-8s | %-25s | %-6s | %-10s | %-26s | %-25s\n","ID","Title","Last Name","First Name","Genre","Quantity","Quality","Rating","ReleaseYear","Publisher/Studio","Illustrator");
+         
+         while (rs.next()) {
+            int id = rs.getInt("bookID");
+            String title = rs.getString("title");
+            String lName = rs.getString("authorLastName");
+            String fName = rs.getString("authorFirstName");
+            String gen = rs.getString("genre");
+            int quan = rs.getInt("quantity");
+            String qual = rs.getString("quality");
+            float rat = rs.getFloat("rating");
+            int relY = rs.getInt("releaseYear");
+            String pubS = rs.getString("publisherStudio");
+            String ill = rs.getString("illustrator");
+            System.out.printf("%-4s | %-35s | %-15s | %-15s | %-25s | %-8s | %-25s | %-6s | %-10s | %-26s | %-25s\n",id, title, lName, fName, gen, quan, qual, rat, relY, pubS, ill);
+            	}
+	        connector.close();
+      		}
+      catch(SQLException e)
+		{
+			System.out.println("Database Error: "+e.getMessage());
+		}
+	}
+	public static void titleSort()
+	{
+	try
+		{
+		String url = "jdbc:mysql://54.84.79.252:3306/sql5664279";
+		Connection connector = DriverManager.getConnection(url,"sql5664279","BD4wVguFkr");
+		Statement stmt = connector.createStatement();
+         	ResultSet rs = stmt.executeQuery("SELECT * FROM books order by title");	
+         	System.out.printf("%-4s | %-35s | %-15s | %-15s | %-25s | %-8s | %-25s | %-6s | %-10s | %-26s | %-25s\n","ID","Title","Last Name","First Name","Genre","Quantity","Quality","Rating","ReleaseYear","Publisher/Studio","Illustrator");
+         
+         while (rs.next()) {
+            int id = rs.getInt("bookID");
+            String title = rs.getString("title");
+            String lName = rs.getString("authorLastName");
+            String fName = rs.getString("authorFirstName");
+            String gen = rs.getString("genre");
+            int quan = rs.getInt("quantity");
+            String qual = rs.getString("quality");
+            float rat = rs.getFloat("rating");
+            int relY = rs.getInt("releaseYear");
+            String pubS = rs.getString("publisherStudio");
+            String ill = rs.getString("illustrator");
+            System.out.printf("%-4s | %-35s | %-15s | %-15s | %-25s | %-8s | %-25s | %-6s | %-10s | %-26s | %-25s\n",id, title, lName, fName, gen, quan, qual, rat, relY, pubS, ill);
+            	}
+	        connector.close();
+      		}
+      catch(SQLException e)
+		{
+			System.out.println("Database Error: "+e.getMessage());
+		}
+	}
+	public static void yearSort()
+	{
+	try
+		{
+		String url = "jdbc:mysql://54.84.79.252:3306/sql5664279";
+		Connection connector = DriverManager.getConnection(url,"sql5664279","BD4wVguFkr");
+		Statement stmt = connector.createStatement();
+         	ResultSet rs = stmt.executeQuery("SELECT * FROM books order by releaseYear");
+         	System.out.printf("%-4s | %-35s | %-15s | %-15s | %-25s | %-8s | %-25s | %-6s | %-10s | %-26s | %-25s\n","ID","Title","Last Name","First Name","Genre","Quantity","Quality","Rating","ReleaseYear","Publisher/Studio","Illustrator");
+         
+         while (rs.next()) {
+            int id = rs.getInt("bookID");
+            String title = rs.getString("title");
+            String lName = rs.getString("authorLastName");
+            String fName = rs.getString("authorFirstName");
+            String gen = rs.getString("genre");
+            int quan = rs.getInt("quantity");
+            String qual = rs.getString("quality");
+            float rat = rs.getFloat("rating");
+            int relY = rs.getInt("releaseYear");
+            String pubS = rs.getString("publisherStudio");
+            String ill = rs.getString("illustrator");
+            System.out.printf("%-4s | %-35s | %-15s | %-15s | %-25s | %-8s | %-25s | %-6s | %-10s | %-26s | %-25s\n",id, title, lName, fName, gen, quan, qual, rat, relY, pubS, ill);
+            	}
+	        connector.close();
+      		}
+      catch(SQLException e)
+		{
+			System.out.println("Database Error: "+e.getMessage());
+		}
+	}
+	public static void rateSort()
+	{
+	try
+		{
+		String url = "jdbc:mysql://54.84.79.252:3306/sql5664279";
+		Connection connector = DriverManager.getConnection(url,"sql5664279","BD4wVguFkr");
+		Statement stmt = connector.createStatement();
+         	ResultSet rs = stmt.executeQuery("SELECT * FROM books order by rating");	
+         	System.out.printf("%-4s | %-35s | %-15s | %-15s | %-25s | %-8s | %-25s | %-6s | %-10s | %-26s | %-25s\n","ID","Title","Last Name","First Name","Genre","Quantity","Quality","Rating","ReleaseYear","Publisher/Studio","Illustrator");
+         
+         while (rs.next()) {
+            int id = rs.getInt("bookID");
+            String title = rs.getString("title");
+            String lName = rs.getString("authorLastName");
+            String fName = rs.getString("authorFirstName");
+            String gen = rs.getString("genre");
+            int quan = rs.getInt("quantity");
+            String qual = rs.getString("quality");
+            float rat = rs.getFloat("rating");
+            int relY = rs.getInt("releaseYear");
+            String pubS = rs.getString("publisherStudio");
+            String ill = rs.getString("illustrator");
+            System.out.printf("%-4s | %-35s | %-15s | %-15s | %-25s | %-8s | %-25s | %-6s | %-10s | %-26s | %-25s\n",id, title, lName, fName, gen, quan, qual, rat, relY, pubS, ill);
+            	}
+	        connector.close();
+      		}
+      catch(SQLException e)
+		{
+			System.out.println("Database Error: "+e.getMessage());
+		}
+	}
     public int getID(){
         return ID;
     }
@@ -87,9 +244,10 @@ public class Book{
         return title;
     }
     public void getDetails()
-    {
-	System.out.println("Title: "+title+"\nCreator: "+authorFName+" "+authorLName+"\nGenre: "+genre+"\nYear of Release: "+releaseYear+"\nRating: "+rating+"\nPublisher/Studio: "+publisherOrStudio+"\nItem Quality: "+quality+"\nIllustrator: "+illustrator+"\nQuantity: "+quantity+"\nQuality: "+quality+"\nID: "+bookID+"\n");
-    }
+	{
+		System.out.println("Title: "+title+"\nCreator: "+authorFName+" "+authorLName+"\nGenre: "+genre+"\nYear of Release: "+releaseYear+"\nRating: "+rating+"\nPublisher/Studio: "+publisherOrStudio+"\nItem Quality: "+quality+"\nIllustrator: "+illustrator+"\nQuantity: "+quantity+"\nQuality: "+quality+"\nID: "+bookID+"\n");
+	}
+
     public void setAuthorFName(String authorFName) {
         this.authorFName = authorFName;
     }

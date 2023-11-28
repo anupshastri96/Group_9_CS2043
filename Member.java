fd6d28dd4c5dd432022e8172d143e294694d1d84
AdamC-4 <145706026+AdamC-4@users.noreply.github.com>
2023-11-28 13:14:57 -0400

    added getAll() display method to Member.java
    prints whole "members" table to the terminal

--- Member.java
+++ Member.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.sql.*;
 public class Member {
     private static int ID =100;
     private final int memberID;
@@ -6,7 +8,33 @@ public class Member {
     private NonBook[] otherRental;
     private String email;
     private double amtOwed;
-
+	
+	public static void getAll()
+	{
+	try
+		{
+		String url = "jdbc:mysql://54.84.79.252:3306/sql5664279";
+		Connection connector = DriverManager.getConnection(url,"sql5664279","BD4wVguFkr");
+		Statement stmt = connector.createStatement();
+         	ResultSet rs = stmt.executeQuery("SELECT * FROM members");	
+         	System.out.printf("%-4s | %-30s | %-11s | %-11s | %-30s | %-8s\n","ID","Name","Books Out","Other Rentals","Email","Ammount Due");
+         
+         while (rs.next()) {
+            int id = rs.getInt("memberID");
+            String n = rs.getString("name");
+            int bOut = rs.getInt("booksOut");
+            int oRen = rs.getInt("otherRental");
+            String em = rs.getString("email");
+            double quan = rs.getDouble("amountOwed");
+            System.out.printf("%-4s | %-30s | %-11s | %-13s | %-30s | %-8s\n",id, n, bOut, oRen, em, quan);
+            	}
+	        connector.close();
+      		}
+      catch(SQLException e)
+		{
+			System.out.println("Database Error: "+e.getMessage());
+		}
+	}
     public Member(String name, OutBook[] booksout, NonBook[] otherRental, String email, double amtOwed) {
         memberID = ID;
         ID++;
@@ -60,7 +88,7 @@ public class Member {
     public void setAmtOwed(double amtOwed) {
         this.amtOwed = amtOwed;
     }
-        public void getDetails()
+       /* public void getDetails()
 	{
 		System.out.println("Name: "+name+"\nEmail: "+email+"\nAmount Owed: $"+amtOwed);
 		if(booksout.length > 0)
@@ -77,5 +105,5 @@ public class Member {
 				System.out.println("Item "+(i+1)+": "+otherRental[i].getTitle());
 			}
 		}
-	}
+	} */
 }
